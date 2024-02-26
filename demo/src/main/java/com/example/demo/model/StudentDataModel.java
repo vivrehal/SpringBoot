990cce59ef090eb6d95d0a41b1739e057e7d0f4c
vivrehal <dhimanvivek777@gmail.com>
2024-02-26 20:34:03 +0530

    initialized spring project and created student model

---
+++ demo/src/main/java/com/example/demo/model/StudentDataModel.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+public class StudentDataModel {
+    public String id;
+    public String name;
+    public String age;
+    public String aadhar;
+    public String university;
+
+}
